bf295e7344479d861195feeda3edb6daaa0c2ed1
Meharban-Singh <meharbansingh10.ms@gmail.com>
2019-12-03 22:49:43 -0500

    server file php without errors

--- server.php
+++ server.php
@@ -1,16 +1,16 @@
 <?php
 
-	$file = $_GET["country"];
-	$dosFile = fopen("dos/" + $file + ".txt", "r");
-	$dontsFile = fopen("donts/" + $file + ".txt", "r");
+	$file = "C:\\xampp\\htdocs\\dos-donts\\dos\\" . $_GET["country"];
+	$dosFile = fopen($file, "r");
+	$dontsFile = fopen($file, "r");
 
 	if($dosFile == false)
 		echo("Error in opening Dos File");
 	if($dontsFile == false)
 		echo("Error in opening Donts File");
 
-	$dosFileText = fread($dosFile,filesize($dosFile));
-	$dontsFileText = fread($dontsFile, filesize($dontsFile));
+	$dosFileText = fread($dosFile,filesize($file));
+	$dontsFileText = fread($dontsFile, filesize($file));
 
 	fclose($dosFile);
 	fclose($dontsFile);
