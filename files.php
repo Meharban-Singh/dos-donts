1cf1a48fc5386da4d24de4cab626a566b639604d
Meharban-Singh <meharbansingh10.ms@gmail.com>
2019-12-11 17:52:40 -0500

    added new system (files.php) which enables us to automatically add all filenames to an array as the files are added in the folder dos, finished verification of user input and converting it to CamelCase and handled errors

---
+++ files.php
@@ -0,0 +1,17 @@
+//https://stackoverflow.com/a/47990744/7976792
+//If link doesnt work -- https://stackoverflow.com/questions/31274329/get-list-of-filenames-in-folder-with-javascript
+
+/*
+a variable 'countries' has is created which gets all the names of files from folder 
+'dos' and makes an array of file names(and hence array of names of all 
+the countries in system).
+*/
+
+let countries = <?php $out = array();
+	foreach (glob('C:\\xampp\\htdocs\\dos-donts\\dos\\*') as $filename) {
+		$p = pathinfo($filename);
+		$out[] = $p['filename'];	
+	}
+
+	echo json_encode($out);
+?>;
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+a variable 'countries' has been obtained form another script (files.php), while
+gets all the names of files from folder 'dos' and makes an array of file names
+(and hence array of names of all the countries in system).
+*/
+
 window.onload = init;
 
 function init() {
@@ -8,19 +14,35 @@ function getData(evt) {
 	evt.preventDefault();
 
 	let country = verifyCountry();
-	let xhr = new XMLHttpRequest();
-	xhr.open('GET', "http://localhost/dos-donts/server.php?country=" + country);
-	xhr.send();
 
-	xhr.onload = () => {
-		let obj = JSON.parse(xhr.response);
-		document.getElementById("doContent").innerHTML = obj['dos'];
-		document.getElementById("dontContent").innerHTML = obj['donts'];
+	if(country != "-1") {
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', "http://localhost/dos-donts/server.php?country=" + country);
+		xhr.send();
+
+		xhr.onload = () => {
+			let obj = JSON.parse(xhr.response);
+			document.getElementById("doContent").innerHTML = obj['dos'];
+			document.getElementById("dontContent").innerHTML = obj['donts'];
+		}
+	}
+	else {
+		alert("Unfortunately, this country is not available in our system yet.");
 	}
 }
 
 function verifyCountry() {
-	let temp = document.getElementsByName("country")[0].value;
-	if(temp == null || temp == undefined || temp == "")
-	return temp;
+	let temp = document.getElementsByName("country")[0].value.trim().split(' ');
+	let out = '';
+
+	for(let i = 0; i < temp.length; i++) {
+		out += temp[i].charAt(0).toUpperCase();
+		out += temp[i].substring(1, temp[i].length).toLowerCase();
+	}
+
+	if(countries.indexOf(out) > -1)
+		return out;
+
+	//In case of error alert the user and dont send request
+	return "-1";
 }
