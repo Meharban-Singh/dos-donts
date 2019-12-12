4e0c5ab20548f5d26780323233088a4077eb8916
Meharban-Singh <meharbansingh10.ms@gmail.com>
2019-12-11 21:47:34 -0500

    added new automatically fillable country bar, removed search button. changed style accordingly

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 /*
-a variable 'countries' has been obtained form another script (files.php), while
+a variable 'countries' has been obtained from another script (files.php), while
 gets all the names of files from folder 'dos' and makes an array of file names
 (and hence array of names of all the countries in system).
 */
-
 window.onload = init;
 
 function init() {
-	document.getElementById("search").addEventListener("submit", getData);
+	autocomplete(document.getElementsByName("country")[0], countries);
 }
 
-function getData(evt) {
-	evt.preventDefault();
-
+function getData() {
 	let country = verifyCountry();
 
 	if(country != "-1") {
@@ -24,6 +21,7 @@ function getData(evt) {
 			let obj = JSON.parse(xhr.response);
 			document.getElementById("doContent").innerHTML = obj['dos'];
 			document.getElementById("dontContent").innerHTML = obj['donts'];
+			document.getElementsByName("country")[0].blur();
 		}
 	}
 	else {
@@ -45,4 +43,99 @@ function verifyCountry() {
 
 	//In case of error alert the user and dont send request
 	return "-1";
+}
+
+
+//https://www.w3schools.com/howto/howto_js_autocomplete.asp
+function autocomplete(inp, arr) {
+  let currentFocus;
+
+  inp.addEventListener("input", function(e) {
+      let a, b, i, val = this.value;
+      
+      closeAllLists();
+
+      if (!val) 
+      	return false;
+      currentFocus = -1;
+      
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+
+      this.parentNode.appendChild(a);
+
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          
+          b.addEventListener("click", function(e) {
+              inp.value = this.getElementsByTagName("input")[0].value;
+              getData();
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+
+  inp.addEventListener("keydown", function(e) {
+      let x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.key === "ArrowDown") {
+        currentFocus++;
+        addActive(x);
+      } else if (e.key === "ArrowUp") {
+        currentFocus--;
+        addActive(x);
+      } else if(e.key === "Tab") {
+      	e.preventDefault();
+      } else if(e.key === "Escape") {
+      	closeAllLists();
+      } else if (e.key === "Enter") {
+        e.preventDefault();
+        if (currentFocus > -1) {
+          if (x) x[currentFocus].click();
+        } else {
+			getData();
+			closeAllLists();    	
+        }
+      }
+  });
+
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+
+    if (currentFocus >= x.length) 
+    	currentFocus = 0;
+
+    if (currentFocus < 0) 
+    	currentFocus = (x.length - 1);
+
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
 }
